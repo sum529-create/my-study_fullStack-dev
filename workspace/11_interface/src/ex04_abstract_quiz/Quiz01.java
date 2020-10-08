25640f043b886ca88521fa82874544e69d9384fa
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-10-08 18:29:47 +0900

    Update Quiz01.java

--- workspace/11_interface/src/ex04_abstract_quiz/Quiz01.java
+++ workspace/11_interface/src/ex04_abstract_quiz/Quiz01.java
@@ -4,14 +4,21 @@ abstract class Fighter{
 	private String name;
 	private boolean isAlive;
 	private int energy;		// 에너지 (100)
-	private int power= (int)(Math.random()*9);		// 공격력(0 ~ 9)
+	private int power;		// 공격력(0 ~ 9)
 	
 	
-	Fighter(String name){
+	
+	public Fighter(String name, int energy, int power) {
 		super();
 		this.name = name;
-		
+		this.energy = energy;
+		this.power = power;
+		isAlive = this.energy > 0; 
 	}
+
+	public abstract void attack(Fighter fighter);
+	// koreanFighter는 20% 확률로 한방에 죽임
+	// ForeignFighter는 10% 확률로 한방에 죽임
 
 	public String getName() {
 		return name;
@@ -21,42 +28,71 @@ abstract class Fighter{
 		this.name = name;
 	}
 
-	public abstract void attack(Fighter fighter);
-	// koreanFighter는 20% 확률로 한방에 죽임
-	// ForeignFighter는 10% 확률로 한방에 죽임
-	
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
 
 	public boolean isAlive() {
 		
-		return false;
+		return isAlive;
 	}
 
+	public void setEnergy(int energy) {
+		this.energy = energy;
+		setAlive(this.energy > 0);		// 연관관계로 살았는지 죽었는지도 알아봄
+	}
 	public int getEnergy() {
 		
 		return energy;
 	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
 	public int getPower() {
-		power = (int)(Math.random()*9);
 		return power;
 		
 	}
 	
 }
 
+
+
 class KoreanFighter extends Fighter{
 	KoreanFighter(String name){
-		super(name);
+		super(name, (int)(Math.random()*101), (int)(Math.random()*10));
 	}
+	@Override
 	public void attack(Fighter fighter) {
+		if(Math.random() < 0.2) {
+			fighter.setEnergy(0);
+			System.out.println(fighter.getName() + " 한 번에 죽었다.");
+		}else {
+			// 얼마만큼 상대편 energy를 줄일 수 있는가? 내 power 만큼
+			// 결론 : 상대편 energy = 상대편 energy - 내 power
+			fighter.setEnergy(fighter.getEnergy() - this.getPower());
+			System.out.println(fighter.getName() + "남은 에너지 : " + fighter.getEnergy());
+			
+		}
 	}
 	
 }
 
 class ForeignFighter extends Fighter{
 	ForeignFighter(String name){
-		super(name);
+		super(name, (int)(Math.random()*101), (int)(Math.random()*10));
 	}
+	@Override
 	public void attack(Fighter fighter) {
+		if(Math.random() < 0.1) {
+			fighter.setEnergy(0);
+			System.out.println(fighter.getName() + " 한 번에 죽었다.");
+		}else {
+			fighter.setEnergy(fighter.getEnergy() - this.getPower());
+			System.out.println(fighter.getName() + "남은 에너지 : " + fighter.getEnergy());
+			
+		}
 		
 	}
 }
@@ -67,6 +103,9 @@ public class Quiz01 {
 		
 		Fighter fighter1 = new KoreanFighter("정찬성");
 		Fighter fighter2 = new ForeignFighter("에드가");
+		
+		System.out.println("fighter1: " + fighter1.getName() + ", 에너지 : " + fighter1.getEnergy() + ", 파워 : " + fighter1.getPower());
+		System.out.println("fighter2: " + fighter2.getName() + ", 에너지 : " + fighter2.getEnergy() + ", 파워 : " + fighter2.getPower());
 		
 		System.out.println("싸우기 시작");
 		
@@ -98,3 +137,4 @@ public class Quiz01 {
 	
 
 }
+
