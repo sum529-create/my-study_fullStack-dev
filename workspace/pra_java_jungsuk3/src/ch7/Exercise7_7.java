3606734e728573f053ef9b3b34ce8d40fde3dda8
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-10-27 15:38:10 +0900

    Update Exercise7_7.java

--- workspace/pra_java_jungsuk3/src/ch7/Exercise7_7.java
+++ workspace/pra_java_jungsuk3/src/ch7/Exercise7_7.java
@@ -1,9 +1,67 @@
 package ch7;
+class Parent {
 
-public class Exercise7_7 {
+	int x = 100;
+
+
+
+	Parent() {
+
+		this(200);
+
+	}
+
+
+
+	Parent(int x) {
+
+		this.x = x;
+
+	}
+
+
+
+	int getX() {
+
+		return x;
+
+	}
+
+}
+
+
+
+class Child extends Parent {
+
+	int x = 3000;
+
+
+
+	Child() {
+
+		this(1000);
+
+	}
+
+
+
+	Child(int x) {
+
+		this.x = x;
+
+	}
+
+}
+
+
+
+class Exercise7_7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Child c = new Child();
+
+		System.out.println("x=" + c.getX());
 
 	}
 
