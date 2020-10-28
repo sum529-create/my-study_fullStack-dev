efbda6b178c43e9b9d2824a8abefa2fde28df12e
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-10-28 18:52:01 +0900

    Add files via upload

---
+++ dbstudy/16. 고급 SQL 작성하기_조인_외부조인사용.sql
@@ -0,0 +1,15 @@
+-- 내부 조인 리뷰
+-- DEPARTMENT와 EMPLOYEE에 모두 존재하는 데이터
+SELECT E.EMP_NO, E.NAME, D.DEPT_NAME, D.LOCATION FROM DEPARTMENT  D, EMPLOYEE S WHERE D.DEPT_NO = E.DEPARTMENT;
+
+-- 외부 조인
+-- 1. LEFT OUTER JOIN : 왼쪽 테이블의 정보는 모두 포함, 오른쪽 테이블의 정보는 일치하는 정보만 포함
+-- 2. RIGHT OUTTER JOIN : 오른쪽 테이블의 정보는 모두 포함, 왼쪽 테이블의 정보는 일치하는 정보만 포함
+
+-- 부서를 아직 배정받지 않은 직원 1명 추가
+INSERT INTO EMPLOYEE VALUES(1005, '김미나', '사원', 'F', 2500000, SYSDATE, NULL);
+
+-- 모든 직원의 사원번호(EMP_NO), 이름(NAME), 부서명(DEPT_NAME)을 조회하시오.
+SELECT E.EMP_NO, E.NAME, D.DEPT_NAME FROM DEPARTMENT D RIGHT OUTER JOIN EMPLOYEE E ON D.DEPT_NO = E.DEPARTMENT;
+
+SELECT E.EMP_NO, E.NAME, D.DEPT_NAME FROM DEPARTMENT D , EMPLOYEE E WHERE D.DEPT_NO = E.DEPARTMENT;
