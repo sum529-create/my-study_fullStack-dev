2742287bee77be97eb60dd321db9d07086747fc6
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-10-28 18:54:51 +0900

    Add files via upload

---
+++ workspace/pra_java_jungsuk3/bin/ch4/Exercise4_11.java
@@ -0,0 +1,22 @@
+package ch4;
+
+public class Exercise4_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 1; 
+		int num2 = 1; 
+		int num3 = 0;
+		System.out.print(num1+","+num2); 
+		
+		for (int i = 0 ; i < 8 ; i++ ) { 
+			num3=num1+num2;
+			System.out.print(","+num3); 
+			
+			num1 = num2;  
+			num2 = num3; 
+		}
+
+	}
+
+}
