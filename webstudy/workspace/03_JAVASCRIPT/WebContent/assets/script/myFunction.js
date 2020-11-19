0beb21df7b605bb055fcc846d9595963a9dd56ac
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-11-19 17:37:59 +0900

    Update myFunction.js

--- webstudy/workspace/03_JAVASCRIPT/WebContent/assets/script/myFunction.js
+++ webstudy/workspace/03_JAVASCRIPT/WebContent/assets/script/myFunction.js
@@ -15,3 +15,31 @@
 		digits || (digits= 0);	// default 처리,   digits의 전달이 없으면 digits은 0으로 처리
 		return Math.round(n * Math.pow(10, digits))/Math.pow(10, digits);
 	}
+
+	// 04_함수/quiz01.html
+	function watch(){
+			setTimeout(function(){
+				var now = new Date();
+				document.getElementById('year').textContent = now.getFullYear();
+				document.getElementById('month').textContent = fillZero(now.getMonth()+1, 2); // 두자리 맞춘다.
+				document.getElementById('date').textContent = fillZero(now.getDate(),2);
+				document.getElementById('hour').textContent = now.getHours();
+				document.getElementById('minute').textContent = fillZero(now.getMinutes(),2);
+				document.getElementById('second').textContent = fillZero(now.getSeconds(),2);
+			
+				// 재귀 호출
+				watch();
+			}, 1000);
+	};	
+	
+
+	// 04_함수/quiz02.html
+	function fillZero(n, digits){
+		digits || (digits = 0);
+		var strN = '' + n;
+		
+		while(strN.length != digits){
+			strN = '0' + strN;
+		}
+		return strN;
+	}
