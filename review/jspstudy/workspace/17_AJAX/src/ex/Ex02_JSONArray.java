99fe8e2a9c77864ee868306c362bc91cdd6c0e1e
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-12-19 23:54:09 +0900

    Update Ex02_JSONArray.java

--- review/jspstudy/workspace/17_AJAX/src/ex/Ex02_JSONArray.java
+++ review/jspstudy/workspace/17_AJAX/src/ex/Ex02_JSONArray.java
@@ -8,26 +8,15 @@ public class Ex02_JSONArray {
 
 	public static void main(String[] args) {
 		
-		// 예제로 사용할 JSON 배열
-		// [{},{}]
-		// [{"name": "emily", "age":20, "isAdult":true}, {"name": "emily", "age":20, "isAdult":true]
-
 		String strJSON = "[{\"name\": \"emily\", \"age\":20, \"isAdult\":true}, {\"name\": \"emily\", \"age\":20, \"isAdult\":true}]";
-		
-		// 1. JSON Parser 생성
 		JSONParser parser = new JSONParser();
-		
-		// 2. JSON Array 준비
 		JSONArray list = null;
 		
-		// 3. parsing
 		try {
 			list = (JSONArray)parser.parse(strJSON);
-		} catch (ParseException e) {
+		}catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		// JSONArray는 Java의 ArrayList와 사용법이 같다.
 		System.out.println(list.get(0));
 		System.out.println(list.get(1));
 		
