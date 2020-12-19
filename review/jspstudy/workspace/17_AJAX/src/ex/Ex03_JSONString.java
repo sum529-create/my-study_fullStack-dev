961d802bda0a493b3859dd28009e356e7d781ffc
sum529-create <72494319+sum529-create@users.noreply.github.com>
2020-12-19 23:56:35 +0900

    Update Ex03_JSONString.java

--- review/jspstudy/workspace/17_AJAX/src/ex/Ex03_JSONString.java
+++ review/jspstudy/workspace/17_AJAX/src/ex/Ex03_JSONString.java
@@ -6,19 +6,14 @@ public class Ex03_JSONString {
 
 	public static void main(String[] args) {
 		
-		// 1. JSON데이터  만들기
-		
 		JSONObject obj = new JSONObject();
 		
-		// Map 처럼 데이터를 저장할 수 있다.
-		obj.put("name", "amanda");
-		obj.put("age", 22);
+		obj.put("name", "amily");
+		obj.put("age", 30);
 		obj.put("isAdult", true);
 		
-		
-		// 2. JSON 데이터를 String으로 변환하기
-		String strJSON = obj.toJSONString();
-		System.out.println(strJSON);
+		String strJson = obj.toJSONString();
+		System.out.println(strJson);
 	}
 
 }
