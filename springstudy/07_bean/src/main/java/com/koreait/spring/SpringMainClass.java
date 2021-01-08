41131d6836b5ddf9df7f19340d55d9b0d8f85003
sum529-create <72494319+sum529-create@users.noreply.github.com>
2021-01-08 18:42:55 +0900

    Add files via upload

--- springstudy/07_bean/src/main/java/com/koreait/spring/SpringMainClass.java
+++ springstudy/07_bean/src/main/java/com/koreait/spring/SpringMainClass.java
@@ -14,7 +14,7 @@ public class SpringMainClass {
 		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 		
 		Student s1 = ctx.getBean("student1", Student.class);
-		Student s2 = ctx.getBean("tudent2", Student.class);
+		Student s2 = ctx.getBean("student2", Student.class);
 		
 		s1.studentInfo();
 		s2.studentInfo();
