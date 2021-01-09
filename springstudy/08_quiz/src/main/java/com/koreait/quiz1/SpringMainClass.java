3517f606ae43947210190afd5cef65fcaf5c4243
sum529-create <72494319+sum529-create@users.noreply.github.com>
2021-01-09 23:46:08 +0900

    Add files via upload

--- springstudy/08_quiz/src/main/java/com/koreait/quiz1/SpringMainClass.java
+++ springstudy/08_quiz/src/main/java/com/koreait/quiz1/SpringMainClass.java
@@ -6,17 +6,16 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class SpringMainClass {
 
 	public static void main(String[] args) {
-		String resourceLocations = "classpath:app-context1.xml"; // classpath: 생략가능
-		AbstractApplicationContext ctx1 = new GenericXmlApplicationContext(resourceLocations);
-		AbstractApplicationContext ctx2 = new AnnotationConfigApplicationContext(AppContext1.class); 
-		Soldier soldier1 = ctx1.getBean("soldier1", Soldier.class);
+		/*String resourceLocations = "app-context1.xml"; // classpath: 생략가능
+		AbstractApplicationContext ctx1 = new GenericXmlApplicationContext(resourceLocations);*/
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext1.class); 
+		Soldier soldier1 = ctx.getBean("soldier1", Soldier.class);
 		// Soldier soldier1 = (Soldier)ctx.getBean("soldier");	// 이렇게 해도 됩니다.
-		Soldier soldier2 = ctx2.getBean("soldier2", Soldier.class);
+		Soldier soldier2 = ctx.getBean("soldier2", Soldier.class);
 		soldier1.infoSolier();
 		System.out.println();
 		soldier2.infoSolier();
-		ctx1.close();
-		ctx2.close();
+		ctx.close();
 	}
 
 }
