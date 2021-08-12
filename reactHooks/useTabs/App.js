f681d30c43eacf7e96177292628f19c0ff6c4b33
sumin <72494319+sum529-create@users.noreply.github.com>
2021-08-12 19:06:44 +0900

    Create App.js

---
+++ reactHooks/useTabs/App.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
+export default function App() {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+}
